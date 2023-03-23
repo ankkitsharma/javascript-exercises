d0d2e668a90812dbbfa1820b67b0f1c26c951b1f
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-23 11:26:12 +0530

    final fix repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,11 @@
-const repeatString = function(string, num) {
-    let result = '';
-    num = 10;
-    for(let i = 0; i < num; i++){
-        result += '';
-}
-return result;
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+  };
+  
+  module.exports = repeatString;
+  

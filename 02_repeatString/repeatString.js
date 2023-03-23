f15285950fb54e62bcaedf24b93cccdafa9a8cdd
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-23 11:22:54 +0530

    fix and run all tests on repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    num = 10;
+    for(let i = 0; i < num; i++){
+        result += '';
+}
+return result;
 };
 
 // Do not edit below this line

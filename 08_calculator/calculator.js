adf6d4c1240237d5528622180c00318a348854ef
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-24 13:38:45 +0530

    fix calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(const item of array) {
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for(const item of array) {
+    product *= item;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  if(num2 !== 0)
+    return(num1 * power(num1, num2 - 1));
+  else
+    return 1;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num !== 0)
+    return num * factorial(num - 1);
+  else
+    return 1;
 };
 
 // Do not edit below this line

26d6a125d200d0b2ae8ebb66cc35d3db8f7d56d5
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-25 12:00:49 +0530

    fix fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(text) {
+    let num = Number(text);
+    if(num < 0)
+        return "OOPS";
+    else if(num == 0 || num == 1)
+        return num;
+    else
+        return fibonacci(num - 1) + fibonacci(num -2);
 };
 
 // Do not edit below this line

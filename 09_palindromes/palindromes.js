e9090469dd27148488db4855ace6c9b9f7ad53a7
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-25 11:41:06 +0530

    fix palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let straightString = string.toLowerCase().replace(/\W/g, '')
+    let reverseString = straightString.split('').reverse().join('');
+    console.log(reverseString);
+    return straightString == reverseString;
 };
 
 // Do not edit below this line

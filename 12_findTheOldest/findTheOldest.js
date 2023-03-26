8242c9d3c4bccb52a621829c02f1b21f0fc616e5
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-26 13:00:59 +0530

    fix findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const currentYear = (new Date()).getFullYear();
+    const oldestPerson = people
+        .map(person => {
+            const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+            return { ...person, age };
+        })
+        .reduce((oldest, curent) => {
+            return curent.age > oldest.age ? curent : oldest;
+        });
+    return oldestPerson;
 };
 
 // Do not edit below this line
